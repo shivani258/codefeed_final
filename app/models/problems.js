f86ee84021d696a54494f3992701e45e72dec67f
shivani258 <shivanisinghpayal@gmail.com>
2015-10-04 00:21:53 +0530

    commit

---
+++ app/models/problems.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose'), Schema = mongoose.Schema;
+var autoIncrement = require('mongoose-auto-increment');
+
+
+var connection = mongoose.createConnection('mongodb://saru95:codefeed@ds061308.mongolab.com:61308/codefeed');
+autoIncrement.initialize(connection);
+
+
+var problemsSchema = mongoose.Schema({
+	problem_name: String,
+	problem_statement: String,
+	problem_input: String,
+	problem_output: String,
+	problem_setter: String,
+	date_added: { type: Date, default: Date.now }
+});
+
+problemsSchema.plugin(autoIncrement.plugin, {
+	model: 'Problems',
+	field: 'problemid'
+});
+
+module.exports = connection.model('Problems', problemsSchema);
+
+
